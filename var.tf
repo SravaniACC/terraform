e76d7b167fe94fb5e898e6c79dd9a0b55fc79651
SravaniACC <47383457+SravaniACC@users.noreply.github.com>
2019-10-15 18:28:18 +0530

    var.tf

---
+++ var.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+ default = "AKIAYLLDGX6MU66R4TM4"
+}
+
+variable "secret_key" {
+    default ="jkovnXNQx1juPXGLu8p6+MnIhOL2L8TjvrfFAA0D"
+}
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "subnet_id" {
+    default ="subnet-06514c363477b9d8b"
+}
+
+variable "key_name" {
+    default ="VDW"
+}
